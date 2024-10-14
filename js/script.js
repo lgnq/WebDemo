7b2e5880aa75c8e475daa494d48580dc0245034a
lgnq <dzzxzz@gmail.com>
2024-10-14 11:14:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,21 +23,22 @@ let orientation = [0, 0, 0];
 let quaternion = [1, 0, 0, 0];
 let calibration = [0, 0, 0, 0];
 
-const maxLogLength = 100;
-const baudRates = [300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000, 500000, 1000000, 2000000];
-const log = document.getElementById('log');
-const butConnect = document.getElementById('butConnect');
-const butClear = document.getElementById('butClear');
-const baudRate = document.getElementById('baudRate');
-const autoscroll = document.getElementById('autoscroll');
+const maxLogLength  = 100;
+const baudRates     = [300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000, 500000, 1000000, 2000000];
+
+const log           = document.getElementById('log');
+const butConnect    = document.getElementById('butConnect');
+const butClear      = document.getElementById('butClear');
+const baudRate      = document.getElementById('baudRate');
+const autoscroll    = document.getElementById('autoscroll');
 const showTimestamp = document.getElementById('showTimestamp');
-const angleType = document.getElementById('angle_type');
-const lightSS = document.getElementById('light');
-const darkSS = document.getElementById('dark');
-const darkMode = document.getElementById('darkmode');
-const canvas = document.querySelector('#canvas');
-const calContainer = document.getElementById('calibration');
-const logContainer = document.getElementById("log-container");
+const angleType     = document.getElementById('angle_type');
+const lightSS       = document.getElementById('light');
+const darkSS        = document.getElementById('dark');
+const darkMode      = document.getElementById('darkmode');
+const canvas        = document.querySelector('#canvas');
+const calContainer  = document.getElementById('calibration');
+const logContainer  = document.getElementById("log-container");
 
 fitToContainer(canvas);
 
@@ -45,6 +46,7 @@ function fitToContainer(canvas){
   // Make it visually fill the positioned parent
   canvas.style.width ='100%';
   canvas.style.height='100%';
+  
   // ...then set the internal size to match
   canvas.width  = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
@@ -84,13 +86,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 async function connect() {
   // - Request a port and open a connection.
   port = await navigator.serial.requestPort();
+  
   // - Wait for the port to open.toggleUIConnected
   await port.open({ baudRate: baudRate.value });
 
   let decoder = new TextDecoderStream();
   inputDone = port.readable.pipeTo(decoder.writable);
-  inputStream = decoder.readable
-    .pipeThrough(new TransformStream(new LineBreakTransformer()));
+  inputStream = decoder.readable.pipeThrough(new TransformStream(new LineBreakTransformer()));
 
   reader = inputStream.getReader();
   readLoop().catch(async function(error) {
@@ -130,22 +132,28 @@ async function disconnect() {
 async function readLoop() {
   while (true) {
     const {value, done} = await reader.read();
+
     if (value) {
       let plotdata;
+
       if (value.substr(0, 12) == "Orientation:") {
         orientation = value.substr(12).trim().split(",").map(x=>+x);
       }
+    
       if (value.substr(0, 11) == "Quaternion:") {
         quaternion = value.substr(11).trim().split(",").map(x=>+x);
       }
+    
       if (value.substr(0, 12) == "Calibration:") {
         calibration = value.substr(12).trim().split(",").map(x=>+x);
+        
         if (!showCalibration) {
           showCalibration = true;
           updateTheme();
         }
       }
     }
+    
     if (done) {
       console.log('[readLoop] DONE', done);
       reader.releaseLock();
@@ -160,14 +168,18 @@ function logData(line) {
     let d = new Date();
     let timestamp = d.getHours() + ":" + `${d.getMinutes()}`.padStart(2, 0) + ":" +
         `${d.getSeconds()}`.padStart(2, 0) + "." + `${d.getMilliseconds()}`.padStart(3, 0);
+
     log.innerHTML += '<span class="timestamp">' + timestamp + ' -> </span>';
+    
     d = null;
   }
+
   log.innerHTML += line+ "<br>";
 
   // Remove old log content
   if (log.textContent.split("\n").length > maxLogLength + 1) {
     let logLines = log.innerHTML.replace(/(\n)/gm, "").split("<br>");
+    
     log.innerHTML = logLines.splice(-maxLogLength).join("<br>\n");
   }
 
@@ -182,21 +194,21 @@ function logData(line) {
  */
 function updateTheme() {
   // Disable all themes
-  document
-    .querySelectorAll('link[rel=stylesheet].alternate')
-    .forEach((styleSheet) => {
+  document.querySelectorAll('link[rel=stylesheet].alternate').forEach((styleSheet) => {
       enableStyleSheet(styleSheet, false);
     });
 
   if (darkMode.checked) {
     enableStyleSheet(darkSS, true);
-  } else {
+  } 
+  else {
     enableStyleSheet(lightSS, true);
   }
 
   if (showCalibration && !logContainer.classList.contains('show-calibration')) {
     logContainer.classList.add('show-calibration')
-  } else if (!showCalibration && logContainer.classList.contains('show-calibration')) {
+  } 
+  else if (!showCalibration && logContainer.classList.contains('show-calibration')) {
     logContainer.classList.remove('show-calibration')
   }
 }
@@ -204,7 +216,6 @@ function updateTheme() {
 function enableStyleSheet(node, enabled) {
   node.disabled = !enabled;
 }
-
 
 /**
  * @name reset
@@ -256,7 +267,6 @@ async function clickTimestamp() {
 async function changeBaudRate() {
   saveSetting('baudrate', baudRate.value);
 }
-
 
 /**
  * @name changeAngleType
@@ -321,17 +331,21 @@ function convertJSON(chunk) {
     let jsonObj = JSON.parse(chunk);
     jsonObj._raw = chunk;
     return jsonObj;
-  } catch (e) {
+  } 
+  catch (e) {
     return chunk;
   }
 }
 
 function toggleUIConnected(connected) {
   let lbl = 'Connect';
+
   if (connected) {
     lbl = 'Disconnect';
   }
+
   butConnect.textContent = lbl;
+  
   updateTheme()
 }
 
@@ -346,15 +360,16 @@ function initBaudRate() {
 
 function loadAllSettings() {
   // Load all saved settings or defaults
-  autoscroll.checked = loadSetting('autoscroll', true);
+  autoscroll.checked    = loadSetting('autoscroll', true);
   showTimestamp.checked = loadSetting('timestamp', false);
-  baudRate.value = loadSetting('baudrate', 9600);
-  angleType.value = loadSetting('angletype', 'quaternion');
-  darkMode.checked = loadSetting('darkmode', false);
+  baudRate.value        = loadSetting('baudrate', 9600);
+  angleType.value       = loadSetting('angletype', 'quaternion');
+  darkMode.checked      = loadSetting('darkmode', false);
 }
 
 function loadSetting(setting, defaultValue) {
   let value = JSON.parse(window.localStorage.getItem(setting));
+
   if (value == null) {
     return defaultValue;
   }
@@ -366,11 +381,11 @@ let isWebGLAvailable = function() {
   try {
     var canvas = document.createElement( 'canvas' );
     return !! (window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
-  } catch (e) {
+  } 
+  catch (e) {
     return false;
   }
 }
-
 
 function updateCalibration() {
   // Update the Calibration Container with the values from calibration
@@ -380,6 +395,7 @@ function updateCalibration() {
     {caption: "Mostly Calibrated",    color: "#FFCC00"},
     {caption: "Fully Calibrated",     color: "#009900"},
   ];
+  
   const calLabels = [
     "System", "Gyro", "Accelerometer", "Magnetometer"
   ]
@@ -407,6 +423,7 @@ camera.position.set(0, 0, 30);
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('black');
+
 {
   const skyColor = 0xB1E1FF;  // light blue
   const groundColor = 0x666666;  // black
@@ -462,7 +479,8 @@ async function render() {
           'YZX'
         );
         bunny.setRotationFromEuler(rotationEuler);
-      } else {
+      } 
+      else {
         let rotationEuler = new THREE.Euler(
           THREE.MathUtils.degToRad(orientation[2]),
           THREE.MathUtils.degToRad(orientation[0]-180),
@@ -471,14 +489,17 @@ async function render() {
         );
         bunny.setRotationFromEuler(rotationEuler);
       }
-    } else {
+    } 
+    else {
       let rotationQuaternion = new THREE.Quaternion(quaternion[1], quaternion[3], -quaternion[2], quaternion[0]);
       bunny.setRotationFromQuaternion(rotationQuaternion);
     }
   }
 
   renderer.render(scene, camera);
+  
   updateCalibration();
+  
   await sleep(10); // Allow 10ms for UI updates
   await finishDrawing();
   await render();
