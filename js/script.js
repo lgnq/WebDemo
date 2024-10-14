195b8ea0edaac50863a6e5ca36601f37a8b8b64d
lgnq <dzzxzz@gmail.com>
2024-10-14 18:12:51 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -243,6 +243,8 @@ function logData(line) {
     log.innerHTML += '<span class="timestamp">' + timestamp + ' -> </span>';
     
     d = null;
+
+    Plotly.extendTraces('plot1', {x:[timestamp], y:[[orientation[0]], [orientation[1]], [orientation[2]]]}, [0, 1, 2], 300);
   }
 
   log.innerHTML += line+ "<br>";
