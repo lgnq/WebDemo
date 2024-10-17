b11577aed18c92dec2f334ea2e03a2514b523900
lgnq <dzzxzz@gmail.com>
2024-10-17 12:07:02 +0800

    add plotly demo again

--- js/script.js
+++ js/script.js
@@ -598,3 +598,23 @@ function saveSetting(setting, value) {
 //   await render();
 // }
 
+function rand() {
+  return Math.random();
+}
+
+Plotly.newPlot('myDiv', [{
+  y: [1,2,3].map(rand),
+  mode: 'lines',
+  line: {color: '#80CAF6'}
+}]);
+
+var cnt = 0;
+
+var interval = setInterval(function() {
+
+  Plotly.extendTraces('myDiv', {
+    y: [[orientations[1]]]
+  }, [0])
+
+  if(++cnt === 100) clearInterval(interval);
+}, 300);
