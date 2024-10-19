a79ccd31ccad62bbd57bfffbd321f5b39cb2daec
lgnq <dzzxzz@gmail.com>
2024-10-19 08:37:25 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -604,11 +604,11 @@ function rand() {
   return Math.random();
 }
 
-Plotly.newPlot('plot1', [{
-  y: [1,2,3].map(rand),
-  mode: 'lines',
-  line: {color: '#80CAF6'}
-}]);
+// Plotly.newPlot('plot1', [{
+//   y: [1,2,3].map(rand),
+//   mode: 'lines',
+//   line: {color: '#80CAF6'}
+// }]);
 
 var cnt = 0;
 
@@ -624,3 +624,4 @@ var interval = setInterval(function() {
 
 // Plotly.newPlot('plot1', data_xyz, layout_xyz, config);
 Plotly.newPlot('myDiv', data_xyz);
+Plotly.newPlot('plot1', data_xyz);
